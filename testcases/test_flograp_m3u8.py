30a5bcb6d86dcda7e041d9a10a0d93b38b0b1e2a
ralphgehy <rgehy84@gmail.com>
2023-06-25 15:56:14 -0400

    added requirements.txt file

--- testcases/test_flograp_m3u8.py
+++ testcases/test_flograp_m3u8.py
@@ -12,8 +12,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains
 
 '''
